9af7199abf788968c6391f12c801447e3a529c52
Apollo <mihai.diaconu02@e-uvt.ro>
2023-07-09 22:54:54 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
 import Main from "./pages/main.jsx"
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, HashRouter } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path="https://apollootty.github.io/wordle-baban/" element={<Main/>} />
-        </Routes>
+      <HashRouter basename="/">
+          <Route path="/" element={<Main/>} />
+        </HashRouter>
     </BrowserRouter>
   );
 }
