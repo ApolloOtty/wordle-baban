d775abe89ef3c0ab5759935e69d89bedb5106220
Apollo <mihai.diaconu02@e-uvt.ro>
2023-07-11 00:42:31 +0300

    Configure React app for deployment to GitHub Pages

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -14,7 +14,7 @@ const App = () => {
 
 
 
-  const [word, setWord] = useState('GIBON');
+  const [word, setWord] = useState('ICTER');
 
 
   const [selectedLetters, setSelectedLetters] = useState([]);
